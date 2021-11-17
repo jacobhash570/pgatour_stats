24d04f4296c05f8340315b3798075be068022234
Jacob <jacob.hash570@gmail.com>
2021-11-17 12:22:15 -0500

    Touch ups.

--- New APp/static/js/app.js
+++ New APp/static/js/app.js
@@ -2,14 +2,14 @@ var tableData = myData;
 
 var $tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
-var inputFieldDate = d3.select("#datetime");
+var inputFieldDate = d3.select("#player");
 //var inputFieldCity = d3.select("#city");
 var columns = ["player", "rounds", "fairway_percentage", "year", "avg_distance", "gir", "avg_putts", "avg_scrambling", "avg_score", "points", "wins", "top_10s", "avg_sg_putts", "avg_sg_total", "sg_ott", "sg_apr", "sg_arg", "money"   ]
 
 var addData = (dataInput) => {
-    dataInput.forEach(ufoSightings => {
+    dataInput.forEach(playerStats => {
         var row = $tbody.append("tr");
-        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        columns.forEach(column => row.append("td").text(playerStats[column])
         )
     });
 }
